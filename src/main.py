d674369e87a07f977062deb194c981955d64d13b
Blazkowich <oiluridze@gmail.com>
2024-08-09 13:58:17 +0400

    new

--- src/main.py
+++ src/main.py
@@ -4,19 +4,21 @@ import numpy as np
 import cv2
 import time
 from load_gif import load_gif_frames
+from PIL import Image
 
 
 def analyze_skin_type():
+    global display_time
     cap = cv2.VideoCapture(0)
 
     cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
     cv2.setWindowProperty('Webcam', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     prediction_text = None
-    display_time = 0
     analysis_done = False
 
-    gif_frames, frame_duration = load_gif_frames(r'../scan_anim.gif')
+    gif_frames, frame_duration = load_gif_frames(r'img/scan_anim.gif')
+    face_percentage_img = cv2.imread(r'img/faces_percentage.png')
 
     while True:
         ret, frame = cap.read()
@@ -59,8 +61,6 @@ def analyze_skin_type():
                         start_time = time.time()
                         while (time.time() - start_time) < 7:
                             for gif_frame in gif_frames:
-                                # noinspection PyUnusedLocal
-                                frame_copy = frame_padded.copy()
                                 gif_frame_resized = cv2.resize(gif_frame, (new_width, new_height))
                                 gif_frame_padded = cv2.copyMakeBorder(
                                     gif_frame_resized,
@@ -92,13 +92,33 @@ def analyze_skin_type():
                             (0, 0, 255), 2, cv2.LINE_AA)
         else:
             if prediction_text and (time.time() - display_time) <= 10:
+                face_img_resized = cv2.resize(face_percentage_img, (new_width, new_height))
+                face_img_padded = cv2.copyMakeBorder(
+                    face_img_resized,
+                    top=(window_height - new_height) // 2,
+                    bottom=(window_height - new_height + 1) // 2,
+                    left=(window_width - new_width) // 2,
+                    right=(window_width - new_width + 1) // 2,
+                    borderType=cv2.BORDER_CONSTANT,
+                    value=(0, 0, 0)
+                )
+
                 text_size, _ = cv2.getTextSize(prediction_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
                 text_x = (window_width - text_size[0]) // 2
                 text_y = (window_height + text_size[1]) // 2
-                cv2.putText(frame_padded, prediction_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (255, 255, 255), 2, cv2.LINE_AA)
-            else:
-                analysis_done = False
+                cv2.putText(face_img_padded, prediction_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (1, 1, 1), 2, cv2.LINE_AA)
+
+                # Display the final image for 15 seconds
+                start_display_time = time.time()
+                while (time.time() - start_display_time) < 15:
+                    cv2.imshow('Webcam', face_img_padded)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        return
+
+                analysis_done = False  # Reset for next analysis
 
         cv2.imshow('Webcam', frame_padded)
         key = cv2.waitKey(1) & 0xFF
